a2b1738e5f9721bc716ddd519a52e53a45c3d23b
Marcin Falkowski <rhobar3@gmail.com>
2015-07-26 04:47:41 +0200

    almost working

--- functions.sh
+++ functions.sh
@@ -7,9 +7,7 @@ source "$DIR/settings.sh"
 declare TRUE_STATUS=0
 declare FALSE_STATUS=1
 
-
 date=`date "+%Y-%m-%dT%H:%M:%S"`
-
 
 
 getCommentTag() {
@@ -31,7 +29,7 @@ isToCloud() {
 	fi
 
 	declare comment
-	comment=$(getCommentTag $filename)
+	comment=$(getCommentTag "$filename")
 	echo "$comment" | grep --quiet -E "$exportMetadataTag(;|$)"
 	return $?
 }
@@ -49,18 +47,49 @@ markAsToCloud() {
 		return $FALSE_STATUS
 	fi
 
-	declare comment=$(getCommentTag $filename)
+	declare comment=$(getCommentTag "$filename")
 
 	exiftool -S -Comment="$comment$exportMetadataTag" "$filename"
+}
+
+
+getAllToCloudFiles() {
+	declare dir="$1"
+	if [ -z "$1" ]; then
+		>&2 echo "ERROR: argument: not passed directory as argument 1"
+		return $FALSE_STATUS
+	fi
+
+	find "${dir}" -type f |\
+	while read filename
+	do
+		isToCloud "$filename" && echo "$filename"
+	done
 }
 
 
 # --files-from=FILE
 #	tylko pliki wymienione w pliku FILE będą transferowane
 
-if false; then
-rsync -av --progress --link-dest="${exportDir}/exportedAll" "${scanDir}" "${exportDir}/exported-${date}" &&
-	ln -sf exported-${date} ${serverPath}/exportedAll
+prepareFilesFromListToCloud() {
+	declare dir="$1"
+	if [[ -z "$1" ]] || [[ ! -f "$1" ]]; then
+		>&2 echo "ERROR: first argument must be a file containing file list"
+		return $FALSE_STATUS
+	fi
 
-rm exiftoolConfig
-fi
+	if [[ ! -d "${exportDir}/exportedCurrent" ]]; then mkdir "${exportDir}/exportedCurrent"; fi
+
+	echo "Preparing files to cloud..."
+	rsync -av --files-from=/tmp/photosExport --progress --link-dest="${exportDir}/exportedCurrent" "${scanDir}" "${exportDir}/exported-${date}" &&
+	ln -sf "exported-${date}" "${exportDir}/exportedCurrent"
+}
+
+prepareFilesToCloud() {
+	declare photosExportFile=/tmp/photosExport
+	echo "Getting flagged files list..."
+	getAllToCloudFiles "${scanDir}" > $photosExportFile
+
+	echo "Prepering files from list to cloud..."
+	prepareFilesFromListToCloud $photosExportFile
+}
